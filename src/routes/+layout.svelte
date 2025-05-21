e0cf270e4d8626fddae512f5e1a8e19eea265196
Rafail Antoniadis <rafail.antoniadis@hotmail.de>
2025-05-21 21:46:05 +0200

    add og

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,21 @@
+<svelte:head>
+  <title>Hat Andy gewonnen?</title>
+  <meta name="description" content="Finde heraus, ob Andys Investment ein Erfolg war." />
+
+  <!-- Open Graph -->
+  <meta property="og:title" content="Hat Andy gewonnen?" />
+  <meta property="og:description" content="Finde heraus, ob Andys Investment ein Erfolg war." />
+  <meta property="og:image" content="https://img.ifunny.co/images/a481f811e8a5adf12dc3c99a759bd569cd5baab71664446e4c77e47aa51ea64b_1.jpg" />
+  <meta property="og:url" content="https://crash-mit-andy.de" />
+  <meta property="og:type" content="website" />
+
+  <!-- Twitter -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Hat Andy gewonnen?" />
+  <meta name="twitter:description" content="Finde heraus, ob Andys Investment ein Erfolg war." />
+  <meta name="twitter:image" content="https://img.ifunny.co/images/a481f811e8a5adf12dc3c99a759bd569cd5baab71664446e4c77e47aa51ea64b_1.jpg" />
+</svelte:head>
+
 <script>
 	import '../app.css';
 </script>
